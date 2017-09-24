cd5b91d52e7cf68e74b446410dd4df78c852cae2
Momojunzi <mark.holthaus@gmail.com>
2017-09-24 08:26:23 -0700

    twitter section working

---
+++ keys.js
@@ -0,0 +1,10 @@
+console.log('this is loaded');
+
+var twitterKeys = {
+	consumer_key: 'hrZNwNCijr5MprQDjT5JOAQnp',
+	consumer_secret: '66kfFkhp7uD8oSKkRD8n3VK4J1hXseY0HprRuTBwzg5EZPZVy1',
+	access_token_key: '911406581400535040-v8F8ZI2JNUxMcvdhVDD1fRynwt1G9TV',
+	access_token_secret: 'eKBTQZWz4iOYXkZT8kc17cEK7IBEoo5c9IOedWLVCPsG6',
+}
+
+module.exports = twitterKeys;
---
+++ liri.js
@@ -0,0 +1,42 @@
+var twitterKeys = require('./keys');
+var moment = require('moment-twitter');
+var Twitter = require('twitter');
+
+var app = {
+	start: function() {
+		var arg = process.argv[2];
+		if (arg === 'my_tweets'){
+			this.twitter();
+		}
+	},
+
+	twitter: function(){
+		var client = new Twitter({
+			consumer_key: twitterKeys.consumer_key,
+			consumer_secret: twitterKeys.consumer_secret,
+		  	access_token_key: twitterKeys.access_token_key,
+		  	access_token_secret: twitterKeys.access_token_secret
+		});
+
+		client.get('statuses/user_timeline', function(error, tweets, response) {		
+			var limit = 20;
+			if(error) {
+				console.log(error);
+			}
+			else {
+				if(tweets.length < 20){
+					limit = tweets.length;
+				}
+				for(var i=0; i<limit; i++) {
+					var date = tweets[i].created_at.split(' ')
+					var hoursAgo = moment(tweets[i].created_at).twitter();
+					date.splice(4, 1);
+					date = date.join(' ');
+					console.log('\ntweet: ' + tweets[i].text + '\ncreated on: ' + date + ' ' + hoursAgo + ' ago');
+				}
+			}
+		});
+	}
+}
+
+app.start();
